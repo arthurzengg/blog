ceba2cd948a2bf1603924a801d9bf7e34f63d16b
arthurzengg <arthurzeng@berkeley.edu>
2023-02-24 00:26:56 -0800

    Models Setup

--- tennisBlog/__init__.py
+++ tennisBlog/__init__.py
@@ -1,7 +1,30 @@
 # tennisBlog/__init__.py
+import os.path
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+
+# Database Setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+# Login Setup
+login_manager = LoginManager()
+# We can now pass in our app to the login manager
+login_manager.init_app(app)
+# Tell users what view to go to when they need to login.
+login_manager.login_view = "users.login"
+
 
 from tennisBlog.core.views import core
 from tennisBlog.error_pages.handlers import error_pages
