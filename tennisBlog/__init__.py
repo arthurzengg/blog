f94ef20666abe19a2fbc591e5f21692d4cb3e592
arthurzengg <arthurzeng@berkeley.edu>
2023-02-26 00:30:03 -0800

    Running site test

--- tennisBlog/__init__.py
+++ tennisBlog/__init__.py
@@ -6,9 +6,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecret'
 
 # Database Setup
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -16,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-Migrate(app,db)
+Migrate(app, db)
 
 # Login Setup
 login_manager = LoginManager()
@@ -27,8 +29,10 @@ login_manager.login_view = "users.login"
 
 
 from tennisBlog.core.views import core
+from tennisBlog.users.views import users
 from tennisBlog.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
 
 
--- tennisBlog/users/views.py
+++ tennisBlog/users/views.py
@@ -8,6 +8,7 @@ from tennisBlog.users.picture_handler import add_profile_pic
 
 users = Blueprint('users', __name__)
 
+## Register
 @users.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -23,6 +24,7 @@ def register():
         return redirect(url_for('users.login'))
     return render_template('register.html', form=form)
 
+# Login
 @users.route('/login', methods=['GET', 'POST'])
 def login():
 
@@ -30,10 +32,6 @@ def login():
     if form.validate_on_submit():
         # Grab the user from our User Models table
         user = User.query.filter_by(email=form.email.data).first()
-
-        # Check that the user was supplied and the password is right
-        # The verify_password method comes from the User object
-        # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
 
         if user.check_password(form.password.data) and user is not None:
             #Log in the user
@@ -53,8 +51,41 @@ def login():
             return redirect(next)
     return render_template('login.html', form=form)
 
-
 @users.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('core.index'))
+
+# Account update Userform
+@users.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+
+    form = UpdateUserForm()
+
+    if form.validate_on_submit():
+        print(form)
+        if form.picture.data:
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data,username)
+            current_user.profile_image = pic
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('User Account Updated')
+        return redirect(url_for('users.account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    profile_image = url_for('static', filename='profile_pics/' + current_user.profile_image)
+    return render_template('account.html', profile_image=profile_image, form=form)
+
+@users.route("/<username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    return render_template('user_blog_posts.html', blog_posts=blog_posts, user=user)
